344ba22e7ceaa400a605f35f448510843cd8f238
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-03-09 15:16:59 -0500

    Agregado el archivo de media

---
+++ estadisticas.py
@@ -0,0 +1,11 @@
+import random
+
+def media(X):
+    return sum(X) / len(X)
+
+
+if __name__ == '__main__':
+    X = [random.randint(1, 21) for i in range(20)]
+    mu = media(X)
+    print(X)  
+    print(mu)
