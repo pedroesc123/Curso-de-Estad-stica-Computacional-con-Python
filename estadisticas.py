fa44874b0b3f085c6bd498b43867b481088cc219
Pedro Espinoza <pedroesc.9905@gmail.com>
2021-03-09 15:32:14 -0500

    Agregado la varianza y desviacion estandar

--- estadisticas.py
+++ estadisticas.py
@@ -1,11 +1,29 @@
 import random
+import math
 
 def media(X):
     return sum(X) / len(X)
+
+def varianza(X):
+    mu = media(X)
+
+    acumulador = 0
+    for x in X:
+        acumulador += (x - mu)**2
+    
+    return acumulador / len(X)
+
+def desviacion_estandar(X):
+    return math.sqrt(varianza(X))
 
 
 if __name__ == '__main__':
     X = [random.randint(1, 21) for i in range(20)]
     mu = media(X)
-    print(X)  
-    print(mu)
+    Var = varianza(X)
+    sigma = desviacion_estandar(X) 
+    
+    print(f'El arreglo X es igual a :{X}')  
+    print(f'La media es igual a: {mu}')
+    print(f'La varianza es igual a: {Var}')
+    print(f'La desviación es igual a: {sigma}')
